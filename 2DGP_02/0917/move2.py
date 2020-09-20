58c216a64b64ec3dfa244a807199d1632e554755
Kiyong Kim <scgyong@gmail.com>
2020-09-21 01:02:10 +0900

    지난 시간까지 한 파일 정리.

--- 2DGP_02/0917/move2.py
+++ 2DGP_02/0917/move2.py
@@ -1,32 +1,36 @@
 from pico2d import *
+from gobj import *
+
+def handle_events():
+    global running
+    evts = get_events()
+    for e in evts:
+        if e.type == SDL_QUIT:
+            running = False
+        elif (e.type, e.key) == (SDL_KEYDOWN, SDLK_ESCAPE):
+            running = False
 
 open_canvas()
 
-gra = load_image('../res/grass.png')
-cha = load_image('../res/character.png')
+grass = Grass()
+team = [ Boy() for i in range(11) ]
 
-x = 0
-while x < 800:
+running = True
+
+while running:
+    # event handling
+    handle_events()
+
+    # game logic
+    for b in team: b.update()
+
+    # game rendering
     clear_canvas()
-    gra.draw(400, 30)
-    cha.draw(x, 85)
+    grass.draw()
+    for b in team: b.draw()
     update_canvas()
-
-    get_events()
-
-    x += 2
 
     delay(0.01)
 
-# delay(2) # in seconds
-# frame animation
 close_canvas()
 
-# game loop:
-#   - update() - logic
-#   - draw() - render
-
-# game loop:
-#   - update() - logic
-#   - event handling
-#   - draw() - render
