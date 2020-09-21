f45ffd8d9e6dead8cfa16a5c5c239aef2b68d74e
Kiyong Kim <scgyong@gmail.com>
2020-09-21 11:37:51 +0900

    added logo_state

---
+++ 2DGP_02/0921/logo_state.py
@@ -0,0 +1,30 @@
+import gfw
+from pico2d import *
+import game_state
+
+def enter():
+    global image
+    image = load_image('../res/kpu_credit.png')
+
+def update():
+    pass
+
+def draw():
+    image.draw(400, 300)
+
+def handle_event(e):
+    if e.type == SDL_QUIT:
+        gfw.quit()
+    elif (e.type, e.key) == (SDL_KEYDOWN, SDLK_ESCAPE):
+        gfw.quit()
+def exit():
+    global image
+    del image
+
+def pause():
+    pass
+def resume():
+    pass
+    
+if __name__ == '__main__':
+    gfw.run_main()
