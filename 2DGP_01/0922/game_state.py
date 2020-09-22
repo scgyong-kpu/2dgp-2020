408f6c9f370b59fd93eeb41508fbb26000e442a9
thlee <watersp@gmail.com>
2020-09-22 13:11:07 +0900

    키 이벤트 처리를 Boy 클래스가 하도록 전달.

--- 2DGP_01/0922/game_state.py
+++ 2DGP_01/0922/game_state.py
@@ -16,28 +16,16 @@ def draw():
 
 def handle_event(e):
     global boy
+    # prev_dx = boy.dx
     if e.type == SDL_QUIT:
         gfw.quit()
     elif e.type == SDL_KEYDOWN:
         if e.key == SDLK_ESCAPE:
             gfw.pop()
-        elif e.key == SDLK_LEFT:
-            boy.dx -= 1
-        elif e.key == SDLK_RIGHT:
-            boy.dx += 1
-        elif e.key == SDLK_DOWN:
-            boy.dy -= 1
-        elif e.key == SDLK_UP:
-            boy.dy += 1
-    elif e.type == SDL_KEYUP:
-        if e.key == SDLK_LEFT:
-            boy.dx += 1
-        elif e.key == SDLK_RIGHT:
-            boy.dx -= 1
-        elif e.key == SDLK_DOWN:
-            boy.dy += 1
-        elif e.key == SDLK_UP:
-            boy.dy -= 1
+
+    boy.handle_event(e)
+    #Boy.handle_event(boy, e)
+
 
 def exit():
     pass
