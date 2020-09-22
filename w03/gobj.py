b75bdaac7b65bd53961f4c08094c5ee2b094c679
Kiyong Kim <scgyong@gmail.com>
2020-09-23 00:12:24 +0900

    키 이벤트에 따라 dx, dy 조정하는 다른 방법 소개
    (cherry picked from commit bc038dfb46fdbd440f3ae5c2ecca90b8620d3202)

--- w03/gobj.py
+++ w03/gobj.py
@@ -13,10 +13,10 @@ class Grass:
 
 class Ball:
     balls = []
-    def __init__(self, x, y, dx, dy):
+    def __init__(self, pos, delta):
         self.image = load_image(RES_DIR + '/ball21x21.png')
-        self.pos = x, y
-        self.delta = dx, dy
+        self.pos = pos
+        self.delta = delta
     def draw(self):
         self.image.draw(*self.pos)
     def update(self):
@@ -24,68 +24,59 @@ class Ball:
         dx,dy = self.delta
         self.pos = x+dx, y+dy
 
+KEY_MAP = {
+    (SDL_KEYDOWN, SDLK_LEFT):  (-1,  0),
+    (SDL_KEYDOWN, SDLK_RIGHT): ( 1,  0),
+    (SDL_KEYDOWN, SDLK_DOWN):  ( 0, -1),
+    (SDL_KEYDOWN, SDLK_UP):    ( 0,  1),
+    (SDL_KEYUP, SDLK_LEFT):    ( 1,  0),
+    (SDL_KEYUP, SDLK_RIGHT):   (-1,  0),
+    (SDL_KEYUP, SDLK_DOWN):    ( 0,  1),
+    (SDL_KEYUP, SDLK_UP):      ( 0, -1),
+}
+KEYDOWN_SPACE = (SDL_KEYDOWN, SDLK_SPACE)
+
 class Boy:
     #constructor
     # def __init__(self, pos, delta):
     # 	self.x, self.y = pos
     # 	self.dx, self.dy = delta
     def __init__(self):
-        self.x = get_canvas_width() // 2
-        self.y = get_canvas_height() // 2
-        self.dx, self.dy = 0, 0
+        self.pos = get_canvas_width() // 2, get_canvas_height() // 2
+        self.delta = 0, 0
         self.fidx = random.randint(0, 7)
         self.image = load_image(RES_DIR + '/animation_sheet.png')
         self.action = 3
     def draw(self):
         sx = self.fidx * 100
         sy = self.action * 100
-        self.image.clip_draw(sx, sy, 100, 100, self.x, self.y)
+        self.image.clip_draw(sx, sy, 100, 100, *self.pos)
     def update(self):
-        # if self.dx < 0:
-        # 	self.action = 0
-        # elif self.dx > 0:
-        # 	self.action = 1
-        # else:
-        # 	self.action = 3
-        self.x += self.dx
-        self.y += self.dy
+        x,y = self.pos
+        dx,dy = self.delta
+        self.pos = x+dx, y+dy
         self.fidx = (self.fidx + 1) % 8
     def fire(self):
-        ball = Ball(self.x, self.y, self.dx, self.dy)
+        ball = Ball(self.pos, self.delta)
         Ball.balls.append(ball)
         print('Ball count = %d' % len(Ball.balls))
     def handle_event(self, e):
-        prev_dx = self.dx
-        if e.type == SDL_KEYDOWN:
-            if e.key == SDLK_LEFT:
-                self.dx -= 1
-            elif e.key == SDLK_RIGHT:
-                self.dx += 1
-            elif e.key == SDLK_DOWN:
-                self.dy -= 1
-            elif e.key == SDLK_UP:
-                self.dy += 1
-            elif e.key == SDLK_SPACE:
-                self.fire()
-        elif e.type == SDL_KEYUP:
-            if e.key == SDLK_LEFT:
-                self.dx += 1
-            elif e.key == SDLK_RIGHT:
-                self.dx -= 1
-            elif e.key == SDLK_DOWN:
-                self.dy += 1
-            elif e.key == SDLK_UP:
-                self.dy -= 1
-
-        if prev_dx != self.dx:
-            if self.dx < 0:
-                self.action = 0
-            elif self.dx > 0:
-                self.action = 1
-            elif prev_dx < 0:
-                self.action = 2
-            elif prev_dx > 0:
-                self.action = 3
+        pair = (e.type, e.key)
+        if pair in KEY_MAP:
+            prev_dx = self.delta[0]
+            dd = KEY_MAP[pair]
+            self.delta = (self.delta[0] + dd[0], self.delta[1] + dd[1])
+            if prev_dx != self.delta[0]:
+                if self.delta[0] < 0:
+                    self.action = 0
+                elif self.delta[0] > 0:
+                    self.action = 1
+                elif prev_dx < 0:
+                    self.action = 2
+                elif prev_dx > 0:
+                    self.action = 3
+        elif pair == KEYDOWN_SPACE:
+            self.fire()
 
 if __name__ == "__main__":
 	print("Running test code ^_^")
