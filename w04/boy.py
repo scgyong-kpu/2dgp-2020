bc0e4099912d6056903286a3ce4552c7a6a54c72
Kiyong Kim <scgyong.kpu@gmail.com>
2020-09-25 13:43:43 +0900

    Week 04 준비

---
+++ w04/boy.py
@@ -0,0 +1,92 @@
+import random
+from pico2d import *
+import gfw_image
+from gobj import *
+
+class Boy:
+    KEY_MAP = {
+        (SDL_KEYDOWN, SDLK_LEFT):  (-1,  0),
+        (SDL_KEYDOWN, SDLK_RIGHT): ( 1,  0),
+        (SDL_KEYDOWN, SDLK_DOWN):  ( 0, -1),
+        (SDL_KEYDOWN, SDLK_UP):    ( 0,  1),
+        (SDL_KEYUP, SDLK_LEFT):    ( 1,  0),
+        (SDL_KEYUP, SDLK_RIGHT):   (-1,  0),
+        (SDL_KEYUP, SDLK_DOWN):    ( 0,  1),
+        (SDL_KEYUP, SDLK_UP):      ( 0, -1),
+    }
+    KEYDOWN_SPACE = (SDL_KEYDOWN, SDLK_SPACE)
+    image = None
+
+    #constructor
+    def __init__(self, rand_pos=False):
+        if rand_pos:
+            self.pos = (
+                random.randint(100, 700),
+                random.randint(100, 500)
+            )
+            self.action = random.randint(0, 3)
+        else:
+            self.pos = get_canvas_width() // 2, get_canvas_height() // 2
+            self.action = 3
+        self.delta = 0, 0
+        self.fidx = random.randint(0, 7)
+        self.target = None
+        self.targets = []
+        self.speed = 0
+        if Boy.image == None:
+            Boy.image = gfw_image.load(RES_DIR + '/animation_sheet.png')
+
+    def draw(self):
+        sx = self.fidx * 100
+        sy = self.action * 100
+        self.image.clip_draw(sx, sy, 100, 100, *self.pos)
+
+    def update(self):
+        x,y = self.pos
+        dx,dy = self.delta
+        self.pos = x+dx, y+dy
+
+        if self.target is not None:
+            ddx = -self.delta[0]
+            helper.move_toward_obj(self)
+            if self.target == None:
+                print("Removing target: ", self.targets[0], " from %d target(s)." % len(self.targets))
+                del self.targets[0]
+                if len(self.targets) > 0:
+                    helper.set_target(self, self.targets[0])
+                    self.updateAction(self.delta[0],0)
+                else:
+                    self.speed = 0
+                    self.updateAction(0, ddx)
+        self.fidx = (self.fidx + 1) % 8
+
+    def fire(self):
+        pass
+
+    def updateDelta(self, ddx, ddy):
+        dx,dy = self.delta
+        dx += ddx
+        dy += ddy
+        if ddx != 0:
+            self.updateAction(dx, ddx)
+        self.delta = dx, dy
+
+    def updateAction(self, dx, ddx):
+        self.action = \
+            0 if dx < 0 else \
+            1 if dx > 0 else \
+            2 if ddx > 0 else 3
+
+    def handle_event(self, e):
+        pair = (e.type, e.key)
+        if pair in Boy.KEY_MAP:
+            if self.target is not None:
+                if e.type == SDL_KEYUP: return
+                self.updateAction(0, -self.delta[0])
+                self.target = None
+                self.delta = 0,0
+                self.targets = []
+                self.speed = 0
+            self.updateDelta(*Boy.KEY_MAP[pair])
+        elif pair == Boy.KEYDOWN_SPACE:
+            self.fire()
