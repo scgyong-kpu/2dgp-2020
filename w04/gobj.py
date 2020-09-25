48651078b4554af262ff2b752cf4df1a04859085
Kiyong Kim <scgyong.kpu@gmail.com>
2020-09-25 15:06:30 +0900

    각 state에 enter/exit 를 두어서 set_state(class_name) 만으로 전환하게 한다.

--- w04/gobj.py
+++ w04/gobj.py
@@ -11,5 +11,8 @@ def point_add(point1, point2):
     x2,y2 = point2
     return x1+x2, y1+y2
 
+def move_obj(obj):
+    obj.pos = point_add(obj.pos, obj.delta)
+    
 if __name__ == "__main__":
 	print("This file is not supposed to be executed directly.")
