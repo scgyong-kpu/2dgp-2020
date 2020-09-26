840228facb7d7db39ccbdb56c3c594a498117503
Kiyong Kim <scgyong.kpu@gmail.com>
2020-09-27 00:49:15 +0900

    enemy generator 를 main_state 로부터 분리.

--- w05/main_state.py
+++ w05/main_state.py
@@ -1,10 +1,9 @@
-import random
 import gfw
 import gfw_world
 from pico2d import *
 from player import Player
 from bullet import LaserBullet
-from enemy import Enemy
+from enemy_gen import EnemyGenerator
 
 canvas_width = 500
 canvas_height = 800
@@ -15,26 +14,12 @@ def enter():
     player = Player()
     gfw_world.add(gfw.layer.player, player)
 
-    set_next_enemy_gen_time()
-
-def set_next_enemy_gen_time():
-    global enemy_gen_time
-    enemy_gen_time = random.uniform(1, 2)
-
-def generate_enemy_if_timed_out():
-    global enemy_gen_time
-    enemy_gen_time -= gfw.delta_time
-    if enemy_gen_time > 0: return
-
-    x = random.randint(0, get_canvas_width())
-    e = Enemy(x, -1)
-    gfw_world.add(gfw.layer.enemy, e)
-    # print('\t\tenemies = ', len(Enemy.enemies))
-    set_next_enemy_gen_time()
+    global enemyGenerator
+    enemyGenerator = EnemyGenerator()
 
 def update():
     gfw_world.update()
-    generate_enemy_if_timed_out()
+    enemyGenerator.update()
 
 def draw():
     gfw_world.draw()
