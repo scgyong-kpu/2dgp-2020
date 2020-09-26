6714eb69b55e4c81e4930fedc3e3ef9c8358407c
Kiyong Kim <scgyong.kpu@gmail.com>
2020-09-27 01:01:45 +0900

    생성기가 wave 를 사용하도록.

--- w05/enemy_gen.py
+++ w05/enemy_gen.py
@@ -5,14 +5,20 @@ from pico2d import *
 from enemy import Enemy
 
 class EnemyGenerator:
+    GEN_X = [ 50, 150, 250, 350, 450 ]
     def __init__(self):
-        self.next_wave = random.uniform(1, 2)
+        self.next_wave = 0
+        self.wave_index = 0
 
     def update(self):
         self.next_wave -= gfw.delta_time
-        if self.next_wave > 0: return
+        if self.next_wave < 0:
+            self.generate_wave()
 
-        x = random.randint(0, get_canvas_width())
-        e = Enemy(x, -1)
-        gfw_world.add(gfw.layer.enemy, e)
-        self.next_wave = random.uniform(1, 2)
+    def generate_wave(self):
+        for x in EnemyGenerator.GEN_X:
+            e = Enemy(x, -1)
+            gfw_world.add(gfw.layer.enemy, e)
+
+        self.wave_index += 1
+        self.next_wave = random.uniform(5, 6)
