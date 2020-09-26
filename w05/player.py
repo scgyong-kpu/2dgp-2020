e672b7994177df8380e4cb7e2d258711045eef93
Kiyong Kim <scgyong.kpu@gmail.com>
2020-09-26 16:15:08 +0900

    x 만 움직이도록 변경. 위치 조정.

--- w05/player.py
+++ w05/player.py
@@ -6,28 +6,33 @@ from gobj import *
 
 class Player:
     KEY_MAP = {
-        (SDL_KEYDOWN, SDLK_LEFT):  (-1,  0),
-        (SDL_KEYDOWN, SDLK_RIGHT): ( 1,  0),
-        (SDL_KEYUP, SDLK_LEFT):    ( 1,  0),
-        (SDL_KEYUP, SDLK_RIGHT):   (-1,  0),
+        (SDL_KEYDOWN, SDLK_LEFT):  -1,
+        (SDL_KEYDOWN, SDLK_RIGHT):  1,
+        (SDL_KEYUP, SDLK_LEFT):     1,
+        (SDL_KEYUP, SDLK_RIGHT):   -1,
     }
     KEYDOWN_SPACE = (SDL_KEYDOWN, SDLK_SPACE)
 
     #constructor
     def __init__(self):
         # self.pos = get_canvas_width() // 2, get_canvas_height() // 2
-        self.pos = 100, 100
-        self.delta = 0, 0
-        self.speed = 0
+        self.x, self.y = 250, 80
+        self.dx = 0
+        self.speed = 3
         self.image = gfw_image.load(RES_DIR + '/fighter.png')
+        half = self.image.w // 2
+        self.minx = half
+        self.maxx = get_canvas_width() - half
 
     def draw(self):
-        self.image.draw(*self.pos)
+        self.image.draw(self.x, self.y)
 
     def update(self):
-        self.pos = point_add(self.pos, self.delta)
+        self.x += self.dx * self.speed
+        if self.x < self.minx: self.x = self.minx
+        elif self.x > self.maxx: self.x = self.maxx
 
     def handle_event(self, e):
         pair = (e.type, e.key)
         if pair in Player.KEY_MAP:
-            self.delta = point_add(self.delta, Player.KEY_MAP[pair])
+            self.dx += Player.KEY_MAP[pair]
