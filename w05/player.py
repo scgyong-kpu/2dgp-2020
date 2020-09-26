8b3c43fedb60c35cffd3e1eff858809b2eabbbf6
Kiyong Kim <scgyong.kpu@gmail.com>
2020-09-26 16:32:10 +0900

    발사 이미지 추가.

--- w05/player.py
+++ w05/player.py
@@ -12,6 +12,8 @@ class Player:
         (SDL_KEYUP, SDLK_RIGHT):   -1,
     }
     KEYDOWN_SPACE = (SDL_KEYDOWN, SDLK_SPACE)
+    LASER_INTERVAL = 0.25
+    SPARK_INTERVAL = 0.05
 
     #constructor
     def __init__(self):
@@ -20,17 +22,30 @@ class Player:
         self.dx = 0
         self.speed = 3
         self.image = gfw_image.load(RES_DIR + '/fighter.png')
+        self.spark = gfw_image.load(RES_DIR + '/laser_0.png')
         half = self.image.w // 2
         self.minx = half
         self.maxx = get_canvas_width() - half
 
+        self.laser_time = 0
+
+    def fire(self):
+        self.laser_time = 0
+
     def draw(self):
         self.image.draw(self.x, self.y)
+        if self.laser_time < Player.SPARK_INTERVAL:
+            self.spark.draw(self.x, self.y + 28)
+
 
     def update(self):
         self.x += self.dx * self.speed
+        self.laser_time += gfw.delta_time
         if self.x < self.minx: self.x = self.minx
         elif self.x > self.maxx: self.x = self.maxx
+
+        if self.laser_time >= Player.LASER_INTERVAL:
+            self.fire()
 
     def handle_event(self, e):
         pair = (e.type, e.key)
