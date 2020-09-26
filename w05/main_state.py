8eb222d8d907385fe32a1413d280fa92dc108c5e
Kiyong Kim <scgyong.kpu@gmail.com>
2020-09-26 23:09:17 +0900

    enemy 추가

--- w05/main_state.py
+++ w05/main_state.py
@@ -1,7 +1,9 @@
+import random
 import gfw
 from pico2d import *
 from player import Player
 from bullet import LaserBullet
+from enemy import Enemy
 
 canvas_width = 500
 canvas_height = 800
@@ -10,11 +12,31 @@ def enter():
     global grass, player
     player = Player()
 
+    set_next_enemy_gen_time()
+
+def set_next_enemy_gen_time():
+    global enemy_gen_time
+    enemy_gen_time = random.uniform(1, 2)
+
+def generate_enemy_if_timed_out():
+    global enemy_gen_time
+    enemy_gen_time -= gfw.delta_time
+    if enemy_gen_time > 0: return
+
+    x = random.randint(0, get_canvas_width())
+    e = Enemy(x, -1)
+    Enemy.enemies.append(e)
+    set_next_enemy_gen_time()
+
 def update():
     player.update()
+    for e in Enemy.enemies: e.update()
     for b in LaserBullet.bullets: b.update()
+    generate_enemy_if_timed_out()
+
 
 def draw():
+    for e in Enemy.enemies: e.draw()
     for b in LaserBullet.bullets: b.draw()
     player.draw()
 
