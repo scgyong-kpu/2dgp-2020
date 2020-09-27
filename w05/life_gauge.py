d8c6f7df2d3650c94c458b54855ca7768d3890e8
Kiyong Kim <scgyong.kpu@gmail.com>
2020-09-27 09:23:37 +0900

    life gauge 그리는 방법 변경.

--- w05/life_gauge.py
+++ w05/life_gauge.py
@@ -8,6 +8,12 @@ def load():
     fg = gfw.image.load(gobj.RES_DIR + '/gauge_fg.png')
 
 def draw(x, y, width, rate):
-    bg.draw(x, y, width, bg.h)
-    w = round(width * rate) - 4
-    fg.draw(x, y, w, fg.h)
+    l = x - width // 2
+    b = y - bg.h // 2
+    draw_3(bg, l, b, width, 3)
+    draw_3(fg, l, b, round(width * rate), 3)
+
+def draw_3(img, l, b, width, edge):
+    img.clip_draw_to_origin(0, 0, edge, img.h, l, b, edge, img.h)
+    img.clip_draw_to_origin(edge, 0, img.w - 2 * edge, img.h, l+edge, b, width - 2 * edge, img.h)
+    img.clip_draw_to_origin(img.w - edge, 0, edge, img.h, l+width-edge, b, edge, img.h)
