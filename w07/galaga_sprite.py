8b79b79ef40c9a2bb5e3133c4f3d32558fa59864
Kiyong Kim <scgyong.kpu@gmail.com>
2020-10-02 11:58:19 +0900

    Animation 추가

--- w07/galaga_sprite.py
+++ w07/galaga_sprite.py
@@ -16,12 +16,25 @@ def load():
                 sprite_rects[name] = tuple(data[name])
 
 class Sprite:
+    FPS = 3
     def __init__(self, name, pos, animates=False):
         load()
         self.name = name
         self.rect = sprite_rects[name]
         self.pos = pos
+        self.animates = animates
+        if animates:
+            self.time = get_time()
+
     def draw(self):
-        galaga_image.clip_draw(*self.rect, *self.pos)
+        if self.animates:
+            frame = round((get_time() - self.time) * Sprite.FPS)
+            if frame % 2 == 0:
+                galaga_image.clip_draw(*self.rect, *self.pos)
+            else:
+                l,b,w,h = self.rect
+                galaga_image.clip_draw(l+w, b, w, h, *self.pos)
+        else:
+            galaga_image.clip_draw(*self.rect, *self.pos)
     def update(self):
         pass
