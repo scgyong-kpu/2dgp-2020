d3f39b0e50ba9a1135eade523f535c6a049f7ab0
Kiyong Kim <scgyong.kpu@gmail.com>
2020-10-12 14:29:25 +0900

    횡스크롤 배경 클래스 만들고 채워넣는 이미지로 표시.

--- w08/background.py
+++ w08/background.py
@@ -120,3 +120,76 @@ class InfiniteBackground(Background):
         dx, dy = x - self.fix_x, y - self.fix_y
         return tx + dx, ty + dy
 
+class HorzScrollBackground:
+    def __init__(self, imageName):
+        self.imageName = imageName
+        self.image = gfw.image.load(res(imageName))
+        self.cw, self.ch = get_canvas_width(), get_canvas_height()
+        self.scroll = 0
+        self.speed = 0
+
+    def update(self):
+        self.scroll += self.speed * gfw.delta_time
+
+    def set_scroll(self, scroll):
+        self.scroll = scroll
+
+    def draw(self):
+        left, bottom = 0, 0
+        page = self.image.w * self.ch // self.image.h
+        curr = int(-self.scroll) % page
+        if curr > 0:
+            sw = int(1 + self.image.h * curr / self.ch)
+            sl = self.image.w - sw
+            src = sl, 0, sw, self.image.h
+            dw = int(sw * self.ch / self.image.h)
+            dst = curr - dw, 0, dw, self.ch
+            self.image.clip_draw_to_origin(*src, *dst)
+        dst_width = round(self.image.w * self.ch / self.image.h)
+        while curr + dst_width < self.cw:
+            dst = curr, 0, dst_width, self.ch
+            self.image.draw_to_origin(*dst)
+            curr += dst_width
+        if curr < self.cw:
+            dw = self.cw - curr
+            sw = int(1 + self.image.h * dw / self.ch)
+            src = 0, 0, sw, self.image.h
+            dw = int(sw * self.ch / self.image.h)
+            dst = curr, 0, dw, self.ch
+            self.image.clip_draw_to_origin(*src, *dst)
+
+    def to_screen(self, point):
+        x, y = point
+        return x - self.scroll, y
+
+    def translate(self, point):
+        x, y = point
+        return x + self.scroll, y
+
+    def get_boundary(self):
+        return (-sys.maxsize, -sys.maxsize, sys.maxsize, sys.maxsize)
+
+    #     self.image.clip_draw_to_origin(*self.src_rect_1, *self.dst_rect_1)
+    #     self.image.clip_draw_to_origin(*self.src_rect_2, *self.dst_rect_2)
+
+    # private void drawHorizontal(Canvas canvas) {
+    #     int left = 0;
+    #     int top = 0;
+    #     int right = UiBridge.metrics.size.x;
+    #     int bottom = UiBridge.metrics.size.y;
+    #     int pageSize = sbmp.getWidth() * (bottom - top) / sbmp.getHeight();
+
+    #     canvas.save();
+    #     canvas.clipRect(left, top, right, bottom);
+
+    #     float curr = scrollX % pageSize;
+    #     if (curr > 0) curr -= pageSize;
+    #     curr += left;
+    #     while (curr < right) {
+    #         dstRect.set(curr, top, curr + pageSize, bottom);
+    #         curr += pageSize;
+    #         canvas.drawBitmap(sbmp.getBitmap(), srcRect, dstRect, null);
+    #     }
+    #     canvas.restore();
+    # }
+
---
+++ w08/horz_state.py
@@ -0,0 +1,47 @@
+import gfw
+from pico2d import *
+from gobj import *
+from player import Player
+from background import HorzScrollBackground
+
+def enter():
+    gfw.world.init(['bg', 'enemy', 'player'])
+
+    center = get_canvas_width() // 2, get_canvas_height() // 2
+    bg = HorzScrollBackground('bg_horz_repeat.png')
+    # bg.speed = 10
+    gfw.world.add(gfw.layer.bg, bg)
+
+    global player
+    player = Player()
+    player.bg = bg
+    gfw.world.add(gfw.layer.player, player)
+
+
+def update():
+    gfw.world.update()
+
+def draw():
+    gfw.world.draw()
+    # gobj.draw_collision_box()
+
+def handle_event(e):
+    # prev_dx = boy.dx
+    if e.type == SDL_QUIT:
+        gfw.quit()
+        return
+    elif e.type == SDL_KEYDOWN:
+        if e.key == SDLK_ESCAPE:
+            gfw.pop()
+            return
+
+    if player.handle_event(e):
+        return
+
+def exit():
+    pass
+
+
+
+if __name__ == '__main__':
+    gfw.run_main()
