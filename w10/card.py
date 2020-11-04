47c98af3e075b488859e84ec2915bfb45f47d523
Kiyong Kim <scgyong.kpu@gmail.com>
2020-11-05 08:59:16 +0900

    toggle fg/bg

--- w10/card.py
+++ w10/card.py
@@ -6,16 +6,20 @@ class Card:
     WIDTH,HEIGHT = 100,100
     def __init__(self, index, pos):
         self.bg = gobj.ImageObject('back.png', pos)
+        self.fg = gobj.ImageObject('fg.png', pos)
+        self.image = self.bg
         self.index = index
     def draw(self):
-        self.bg.draw()
+        self.image.draw()
     def update(self):
         pass
     def handle_event(self, e):
         if e.type == SDL_MOUSEBUTTONDOWN and e.button == SDL_BUTTON_LEFT:
             pos = gobj.mouse_xy(e)
             if gobj.pt_in_rect(pos, self.get_bb()):
-                print("It's me:", self.index)
+                self.toggle()
+    def toggle(self):
+        self.image = self.bg if self.image == self.fg else self.fg
     def get_bb(self):
         hw = Card.WIDTH // 2
         hh = Card.HEIGHT // 2
