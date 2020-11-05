2f12a8932df3d10ba9f06734996f3fc43cba33b1
Kiyong Kim <scgyong.kpu@gmail.com>
2020-11-05 10:09:37 +0900

    score 표시

--- w10/pair_state.py
+++ w10/pair_state.py
@@ -11,6 +11,10 @@ canvas_height = PADDING + (Card.WIDTH + PADDING) * 4 + SCORE_HEIGHT
 
 print("Canvas Size:", (canvas_width, canvas_height))
 
+score_x = PADDING
+score_y = canvas_height - SCORE_HEIGHT * 8 // 10
+SCORE_TEXT_COLOR = (255, 255, 255)
+
 start_x = Card.WIDTH // 2 + PADDING
 start_y = Card.HEIGHT // 2 + PADDING
 
@@ -35,11 +39,23 @@ def enter():
     global last_card
     last_card = None
 
+    global score, font
+    score = 0
+    font = gfw.font.load(gobj.res('ENCR10B.TTF'), 20)
+
 def update():
     gfw.world.update()
 
+    global score
+    if gfw.world.count_at(gfw.layer.card) > 0:
+        score += gfw.delta_time
+        # print(gfw.world.count_at(gfw.layer.card), score, gfw.delta_time, gfw.frame_interval)
+
 def draw():
     gfw.world.draw()
+
+    global score, font
+    font.draw(score_x, score_y, "Score: %.1f" % score, SCORE_TEXT_COLOR)
 
 def handle_event(e):
     if e.type == SDL_QUIT:
